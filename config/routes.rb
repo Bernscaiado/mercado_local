4821f1d5831789a65c4f10929fd5cdb8afa1eed0
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2020-08-24 17:46:05 -0300

    user add product

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do 
-  resources :products, only: [:index]
+Rails.application.routes.draw do
+  resources :products, only: %i[index show new create]
   get 'products/index'
   devise_for :users
   root to: 'pages#home'
