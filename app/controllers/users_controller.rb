ef11583fab8ef91ec3d70aea91de58f8cd330b9e
Morgana Borges <morgana.t.borges@gmail.com>
2020-08-26 20:09:48 -0300

    map adicionado mas ainda não funciona

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where(user.role == true)
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat }),
+        image_url: helpers.asset_url('icon.jpg')
+      }
+    end
   end
 
   def show
