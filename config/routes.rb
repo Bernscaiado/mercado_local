167873acb14ef2f8c6c2a5553a0442f80434d71a
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2020-08-26 13:26:24 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   end
   get 'products/index'
   devise_for :users
-  get 'total', to: 'carts#total'
   get 'search', to: 'pages#search'
   get 'myprofile', to: 'pages#profile'
   get 'producers', to: 'pages#producer'
